ee84fba680f728f081c06406cf3d3ee0cda1fe99
mingyu <vkans7526@naver.com>
2023-01-14 10:36:56 +0900

    변경사항

--- calc_test2.py
+++ calc_test2.py
@@ -1,9 +1,15 @@
-from calculator import plus ## 모듈 import 하는방법 두번째
+from calculator import plus # 모듈 import 하는방법 두번째
 from calculator import minus
 from calculator import mul
 
+#모듈 import 하는 여러가지 방법
+# from calculator import plus,minus,mul,div
+# form claculator import * 
 
 print(plus(2,3))
 print(minus(10,5))
 print(mul(2,5))
-print(div(10,1)) ## from calculator import div 하지 않아서 실행안됨
+print(div(10,1)) # from calculator import div 하지 않아서 실행안됨
+
+# from calculator import c
+# print(c.plus(1,2))
---
+++ my_packge/calc_test3.py
@@ -0,0 +1,6 @@
+from my_packge import calculator
+
+print(calculator.plus(10,5))
+print(calculator.minus(10,2))
+print(calculator.mul(10,5))
+print(calculator.div(10,2))
