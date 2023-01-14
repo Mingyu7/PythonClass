fb67ef32e199598887c3bd15789e623520eb4839
MinGyu1 <vkans7526@naver.com>
2023-01-14 09:50:51 +0900

    모듈사용법

---
+++ calc_test.py
@@ -0,0 +1,7 @@
+import calculator ##모듈의 파일 이름 *확장자 제외*
+
+## 모듈이름.함수() 로 사용
+print(calculator.plus(1,3))
+print(calculator.minus(10,5))
+print(calculator.mul(2,3))
+print(calculator.div(10,2))
---
+++ calc_test2.py
@@ -0,0 +1,9 @@
+from calculator import plus ## 모듈 import 하는방법 두번째
+from calculator import minus
+from calculator import mul
+
+
+print(plus(2,3))
+print(minus(10,5))
+print(mul(2,5))
+print(div(10,1)) ## from calculator import div 하지 않아서 실행안됨
